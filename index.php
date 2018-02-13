c89eb16fb69b5fcb6a2cfb9dc6d671fe163e3d8f
Justin Argyle <justinargyle@u.boisestate.edu>
2018-02-13 15:18:45 -0700

    testing basic landing page

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <?php
 $thisPage = "Landing";
 include_once ("lib/pageHeader.php");
+include_once ("lib/travelBar.php");
 include_once ("lib/footer.php");
 echo ("Hello World")
 ?>
--- lib/travelBar.php
+++ lib/travelBar.php
@@ -4,20 +4,25 @@
     echo " id=\"campaignBar\" class=\"travelBar\"> ";
 }else if ($thisPage=="Videos"){
     echo "id=\"videoBar\" class=\"travelBar\">";
+}else if ($thisPage=="Landing"){
+    echo "id=\"landingBar\" class=\"travelBar\">";
 }?>
 <div class="centerList">
 <ol class="inlineList">
-    <?php if ($thisPage=="CreaturesNCharacters"){
+    <?php if ($thisPage=="Landing") {
+        echo "<li> <div class=\"listSurround\"><a href=\"/content/Campaigns.php\"><div class=\"travelList\" >Visit as Guest</div></a></div></li> ";
+        echo "<li> <div class=\"listSurround\"><a href=\"/Form.html\"><div class=\"travelList\">Login</div></a></div></li>";
+    }else if ($thisPage=="CreaturesNCharacters"){
         echo "<li> <div class=\"currentSurround\"><div class=\"travelList\" >Creatures</div></div></li> ";
-        echo "<li> <div class=\"listSurround\"><a href=\"Campaigns.php\"><div class=\"travelList\">Campaigns</div></a></div></li>";
-        echo "<li> <div class=\"listSurround\"><a href=\"Videos.php\"><div class=\"travelList\">Videos</div></a></div></li>";
+        echo "<li> <div class=\"listSurround\"><a href=\"/content/Campaigns.php\"><div class=\"travelList\">Campaigns</div></a></div></li>";
+        echo "<li> <div class=\"listSurround\"><a href=\"/content/Videos.php\"><div class=\"travelList\">Videos</div></a></div></li>";
     }else if ($thisPage=="Campaigns"){
         echo "<li> <div class=\"listSurround\"><a href=\"CreaturesNCharacters.php\"><div class=\"travelList\" >Creatures</div></a></div></li> ";
         echo "<li> <div class=\"currentSurround\"><div class=\"travelList\">Campaigns</div></div></li>";
-        echo "<li> <div class=\"listSurround\"><a href=\"Videos.php\"><div class=\"travelList\">Videos</div></a></div></li>";
+        echo "<li> <div class=\"listSurround\"><a href=\"/content/Videos.php\"><div class=\"travelList\">Videos</div></a></div></li>";
     }else if ($thisPage=="Videos"){
-        echo "<li> <div class=\"listSurround\"><a href=\"CreaturesNCharacters.php\"><div class=\"travelList\" >Creatures</div></a></div></li> ";
-        echo "<li> <div class=\"listSurround\"><a href=\"Campaigns.php\"><div class=\"travelList\">Campaigns</div></a></div></li>";
+        echo "<li> <div class=\"listSurround\"><a href=\"/content/CreaturesNCharacters.php\"><div class=\"travelList\" >Creatures</div></a></div></li> ";
+        echo "<li> <div class=\"listSurround\"><a href=\"/content/Campaigns.php\"><div class=\"travelList\">Campaigns</div></a></div></li>";
         echo "<li> <div class=\"currentSurround\"><div class=\"travelList\">Videos</div></a></div></li>";
     }
 ?>
